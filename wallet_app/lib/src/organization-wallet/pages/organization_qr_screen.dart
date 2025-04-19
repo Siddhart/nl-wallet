29e249622032e6f77a91144fac4fdebe0a0248c5
Siddhart <siddhartssg@gmail.com>
2025-04-19 21:04:11 +0200

    login http request

--- wallet_app/lib/src/organization-wallet/pages/organization_qr_screen.dart
+++ wallet_app/lib/src/organization-wallet/pages/organization_qr_screen.dart
@@ -1,19 +1,27 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../../../environment.dart';
+import '../../data/source/wallet_datasource.dart';
+import '../../navigation/wallet_routes.dart';
 import '../../util/extension/build_context_extension.dart';
 import '../../util/extension/string_extension.dart';
 import '../../feature/common/widget/button/bottom_back_button.dart';
 import '../../feature/common/widget/centered_loading_indicator.dart';
 import '../../feature/common/widget/wallet_app_bar.dart';
 
+import 'package:http/http.dart' as http;
+
 final _scannerKey = Environment.isTest ? ValueKey(DateTime.now()) : GlobalKey();
 
 class OrganizationQrScreen extends StatefulWidget {
   final String organizationId;
-  const OrganizationQrScreen({Key? key, required this.organizationId}) : super(key: key);
+  const OrganizationQrScreen({Key? key, required this.organizationId})
+      : super(key: key);
 
   @override
   State<OrganizationQrScreen> createState() => _OrganizationQrScreenState();
@@ -72,9 +80,27 @@ class _OrganizationQrScreenState extends State<OrganizationQrScreen> {
         MobileScanner(
           key: _scannerKey,
           controller: cameraController,
-          onDetect: (capture) {
+          onDetect: (capture) async {
             final barcode = capture.barcodes.first;
-            print('Scanned QR code: ${barcode.rawValue} for organization: ${widget.organizationId}');
+
+            final source = context.read<WalletDataSource>();
+            final cards = await source.readAll();
+
+            final attributes = cards.first.attributes;
+            if (barcode.rawValue != null) {
+              final response = await http.post(Uri.parse(barcode.rawValue!),
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                  },
+                  body: json.encode(cards.first));
+
+              print(response);
+
+              // Navigator.restorablePushNamed(
+              //     context, WalletRoutes.organizationWalletRoute,
+              //     arguments: {'id': widget.organizationId});
+            }
           },
           errorBuilder: (context, error, child) {
             return Center(
@@ -111,4 +137,4 @@ class _OrganizationQrScreenState extends State<OrganizationQrScreen> {
       ],
     );
   }
-} 
+}
